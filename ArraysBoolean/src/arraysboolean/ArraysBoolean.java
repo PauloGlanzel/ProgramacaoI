13224e78906f727933f722a0a657303c26b7caf0
Paulo Henrique Glanzel <pauloglanzel@hotmail.com>
2021-01-04 17:58:13 -0300

    1.0.0.2

--- ArraysBoolean/src/arraysboolean/ArraysBoolean.java
+++ ArraysBoolean/src/arraysboolean/ArraysBoolean.java
@@ -12,12 +12,14 @@ public class ArraysBoolean {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        boolean[] estudAprovProgI = {true, false, true, false, true};
-        /*
+        Boolean[] estudAprovProgI = new Boolean[5];
+        int a = 1;
+        
         for(int i=0; i < estudAprovProgI.length; i++){
-            System.out.print("Digite 'true' ou 'false' para o aluno "+i+".: ");
-            estudAprovProgI[i] = Boolean.getBoolean(sc.next());
-        }*/
+            System.out.print("Digite 'true' ou 'false' para o "+a+"º aluno.: ");
+            estudAprovProgI[i] = sc.nextBoolean();
+            a += 1;
+        }
         System.out.println("");
         System.out.println("Valores digitados.:");
         for(int i=0; i < estudAprovProgI.length; i++){
